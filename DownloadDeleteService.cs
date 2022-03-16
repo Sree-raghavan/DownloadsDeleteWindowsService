d1dfe2025d87654b927422911dee82a8b483b8fd
Sreeraghavan anandan <sriraghavan98@gmail.com>
2022-03-16 20:18:24 +0530

    Add project files.

---
+++ DownloadDeleteService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Timers;
+
+namespace DownloadsDeleteService
+{
+    class DownloadDeleteService
+    {
+        private readonly Timer _timer;
+        public DownloadDeleteService()
+        {
+            //Mentioning timer interval i have set for every 12 hrs from service staring
+            _timer = new Timer(12*60*60 * 1000) { AutoReset = true };
+            _timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var path = ConfigurationManager.AppSettings["downloadDirectoryFilePath"];
+                string[] Files = Directory.GetFiles(@""+path);
+                foreach(var file in Files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    if(fi.CreationTime<DateTime.Now.Subtract(TimeSpan.FromDays(7)))
+                    {
+                        WriteLogFile("Deleted:" + fi.Name);
+                        File.Delete(file);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLogFile("Exception caught: Message=" + ex.Message);
+                Stop();
+                throw;
+            }
+        }
+
+        public void Start()
+        {
+            WriteLogFile("Service is Started!");
+            _timer.Start();
+
+        }
+        public void Stop()
+        {
+            WriteLogFile("Service is Stopped!");
+            _timer.Stop();
+        }
+        public void WriteLogFile(string message)
+        {
+            var logpath = ConfigurationManager.AppSettings["logFilePath"];
+            StreamWriter sw = null;
+            string date = DateTime.Now.Date.ToString("ddMMyyyy");
+            sw = new StreamWriter(@"" + logpath + @"\Log-" + date + ".txt", true);
+            sw.WriteLine($"{DateTime.Now.ToString()} : {message}");
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Topshelf;
+
+namespace DownloadsDeleteService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<DownloadDeleteService>(s =>
+                {
+                    s.ConstructUsing(EhsService => new DownloadDeleteService());
+                    s.WhenStarted(EhsService => EhsService.Start());
+                    s.WhenStopped(EhsService => EhsService.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("MyDownloadFolderDeleteService");//name for the service
+                x.SetDisplayName("My Download older Delete Service");//name to be displayed
+                //desciption of the windows service!
+                x.SetDescription("My personal windows service created to delete downloads folder files after x days");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
